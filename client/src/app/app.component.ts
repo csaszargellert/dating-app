c2906c6afa15d6ec3f99fdeead3948a40345dec2
Gellért Császár <csaszargellert@gmail.com>
2024-01-28 13:31:23 +0100

    register toastr and bootswatch library

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
+import { AccountService } from './services/account.service';
+import { ApiResponseUser } from './interfaces/ApiResponseUser';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,26 @@ export class AppComponent implements OnInit {
   title = 'Dating App';
   users: any;
   private http: HttpClient = inject(HttpClient);
+  private account: AccountService = inject(AccountService);
 
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  getUsers() {
     this.http.get('https://localhost:5001/api/users').subscribe({
       next: (users) => (this.users = users),
       error: (error) => console.error(error),
       complete: () => console.log('This has completed in APP_COMPONENT'),
     });
+  }
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+
+    const user: ApiResponseUser = JSON.parse(userString);
+    this.account.setCurrentUser(user);
   }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { ToastrModule } from 'ngx-toastr';
+import { ErrorsComponent } from './errors/errors.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
+    ListsComponent,
+    MessagesComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ErrorsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      timeOut: 2000,
+      progressBar: true,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
